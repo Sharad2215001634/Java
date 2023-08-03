ebbb9e4a9376219bfeea9ad0c114a95cbaa95036
Sharad kumar Singh <122071298+Sharad2215001634@users.noreply.github.com>
2023-08-03 22:24:49 +0530

    Add files via upload

---
+++ JAVA c/area.java
@@ -0,0 +1,9 @@
+class area {
+    public static void main(String[] args) {
+        float area;
+        int r=10;
+        float i= 3.14;
+        area= i*r*r;
+        System.out.println("Arfea is : %f",area); 
+    }
+}
---
+++ JAVA c/first.java
@@ -0,0 +1,9 @@
+class first 
+{
+    public static void main(String[] args)
+     {
+        System.out.println("HOla  Buddy !"); 
+        System.out.println("Name - Sharad "); 
+        System.out.println("Roll number - 2215002634"); 
+    }
+}
